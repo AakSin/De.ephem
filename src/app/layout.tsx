e8d98052ce56e198ca64c5c0feef21eebfa0e25f
AakSin <29751759+AakSin@users.noreply.github.com>
2024-03-22 17:26:51 +0400

    fixed favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,24 @@ export default function RootLayout({
       <meta property="og:image:type" content="<generated>" />
       <meta property="og:image:width" content="<generated>" />
       <meta property="og:image:height" content="<generated>" />
+      <link rel="shortcut icon" href="/images/favicon.ico" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/images/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/images/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/images/favicon-16x16.png"
+      />
       <body className={inter.className}>{children}</body>
     </html>
   );
